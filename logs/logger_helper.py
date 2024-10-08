4536adb898e9dfb640e790ae014f5ec25e3498c7
LeoMelaverde <leo.martinez@melaverde.io>
2024-10-08 14:28:55 +0200

    adjusted the file path for writing the log

--- logs/logger_helper.py
+++ logs/logger_helper.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class LoggerHelper:
     def __init__(self, file: str):
         self.file_name = os.path.basename(os.path.dirname(os.path.abspath(file)))
-        self.file_path = f"{Path(__file__).resolve().parent}/"
+        self.file_path = f"{Path(file).resolve().parent}/"
 
     def get_logger(self, name: str) -> logging.Logger:
         logger = logging.getLogger(name)
